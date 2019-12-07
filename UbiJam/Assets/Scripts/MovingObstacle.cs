69d8a1514c26e6412814544187468af775113d8b
Iulian <iuliansusai@gmail.com>
2019-12-07 17:34:41 +0200

    Update levels

---
+++ UbiJam/Assets/Scripts/MovingObstacle.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObstacle : MonoBehaviour
+{
+    [SerializeField] private float moveDelay;
+    [SerializeField] private float moveTime;
+    [SerializeField] private AnimationCurve moveCurve;
+    [SerializeField] private GameObject movingObst;
+    [SerializeField] private List<GameObject> movePoints;
+
+    private bool isMoving;
+    private float startMoveTime;
+    private Vector3 startPos;
+    private Vector3 endPos;
+    private int moveIndex;
+    private void Start() {
+        moveIndex = 0;
+        movingObst.transform.position = movePoints[0].transform.position;
+        StartMove();
+    }
+
+    private void StartMove() {
+        moveIndex++;
+        Transform nextMove = movePoints[moveIndex % movePoints.Count].transform;
+        isMoving = true;
+        startMoveTime = Time.time;
+        startPos = movingObst.transform.position;
+        endPos = nextMove.position;
+    }
+
+    private void UpdateMove() {
+        float timeSinceStart = Time.time - startMoveTime;
+        float percentage = timeSinceStart / moveTime;
+        float curve = moveCurve.Evaluate(percentage);
+        movingObst.transform.position = Vector3.Lerp(startPos, endPos, curve);
+        if(percentage >= 1.0f) {
+            isMoving = false;
+            movingObst.transform.position = endPos;
+            Invoke("StartMove", moveDelay);
+        }
+    }
+
+    private void Update() {
+        if (isMoving) {
+            UpdateMove();
+        }
+    }
+
+
+}
---
+++ UbiJam/Assets/Scripts/ScalingObstacle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScalingObstacle : MonoBehaviour
+{
+    [SerializeField] private float scaleTime;
+    [SerializeField] private Vector3 startScale;
+    [SerializeField] private Vector3 endScale;
+    [SerializeField] private AnimationCurve scaleCurve;
+    [SerializeField] private List<GameObject> scalingObstacles;
+
+    private bool isScaling;
+    private float scaleStartTime;
+
+    private void Start() {
+        StartScaling();
+    }
+
+    private void StartScaling() {
+        isScaling = true;
+        scaleStartTime = Time.time;
+        foreach (GameObject go in scalingObstacles) {
+            go.transform.localScale = startScale;
+        }
+    }
+
+    private void UpdateScale() {
+        float timeSinceStrart = Time.time - scaleStartTime;
+        float percentage = timeSinceStrart / scaleTime;
+        float curve = scaleCurve.Evaluate(percentage);
+
+        foreach(GameObject go in scalingObstacles) {
+            go.transform.localScale = Vector3.Lerp(startScale, endScale, curve);
+        }
+
+        if(percentage >= 1.0f) {
+            isScaling = false;
+        }
+
+    }
+
+    private void Update() {
+        if (isScaling) {
+            UpdateScale();
+        }
+    }
+
+}
